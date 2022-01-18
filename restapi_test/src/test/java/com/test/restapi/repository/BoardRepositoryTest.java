e0fe14a45556e92866bce55125cf5bacf6be23f4
LG <LG@DESKTOP-U3AACCL>
2022-01-19 01:25:54 +0900

    자바 로직별 단위/통합테스트 소스 추가

--- restapi_test/src/test/java/com/test/restapi/repository/BoardRepositoryTest.java
+++ restapi_test/src/test/java/com/test/restapi/repository/BoardRepositoryTest.java
@@ -2,10 +2,10 @@ package com.test.restapi.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//BoardReopository 메소드 단위/통합 테스트
 public class BoardRepositoryTest {
 	
 	@Autowired
